bb1bf309fb314078c2c198cd7c87dc8e80bbbb25
Santosh Marigowda <santosh.marigowda@sky.uk>
2016-12-08 11:04:33 +0000

    12 events representing one hour stats

--- riemann-java-client/src/test/java/riemann/java/client/tests/MyTest.java
+++ riemann-java-client/src/test/java/riemann/java/client/tests/MyTest.java
@@ -7,11 +7,11 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("my test for Riemann client...");
 
-        for( int i=0; i<10; i++) {
+        for( int i=0; i<17; i++) {
 
             double[] countOK = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 5.0, 0, 0 }; // 2 out of 10 (1 out of 5)-- ok
             double[] countWarning = { 0, 0, 0, 0, 12, 11, 10, 10, 9, 10, 0, 0 }; // 6 out of 10 (3 out of 5) -- warning
-            double[] countCritical = { 0, 0, 11, 11, 11, 10, 10, 10, 9, 10, 11, 11 }; // 8 out of 10 (2 out of 5)-- critical
+            double[] countCritical = { 0, 0, 11, 11, 11, 10, 10, 10, 9, 10, 11, 11, 11, 11, 11, 11, 11 }; // 8 out of 10 (2 out of 5)-- critical
 
             double[] count;
 //            count = countOK;
